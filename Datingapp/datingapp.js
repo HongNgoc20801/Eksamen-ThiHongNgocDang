e34c794747d6af434cc296ee298173e0a671a33e
HongNgoc20801 <hongngoc20082001@gmail.com>
2025-05-09 19:30:53 +0200

    make idea and fix UI of dating page , which page go in after login

--- Datingapp/datingapp.js
+++ Datingapp/datingapp.js
@@ -3,8 +3,38 @@ if(!userLoggedIn){
     alert("Please log in to continue using the app.");
     window.location.href="/Login/login.html";
 }else{
-    const userProfil = document.getElementById("information-user");
-    userProfil.innerHTML = `
-        <p>Welcome to Dating App, where you can connnect with the other even how far it is, <strong>${userLoggedIn.name}</strong>!</p>
+    document.getElementById("information-user").innerHTML = `
+    <p>Welcome to Dating App, where you can connnect with the other even how far it is, <strong>${userLoggedIn.name}</strong>!</p>
     `;
-}   
+    showSection("home")
+} 
+//Nav  
+document.getElementById("home").addEventListener("click",()=>showSection("home"));
+document.getElementById("information").addEventListener("click",()=>{
+    showSection("information");
+    showUserInformation(userLoggedIn);
+});
+document.getElementById("swipe").addEventListener("click",()=> showSection("swipe"));
+document.getElementById("liked").addEventListener("click",()=> showSection("liked"));
+
+function showSection(sectionId){
+    const section =["home", "profile", "swipe", "liked"];
+    section.forEach((id)=> {
+        const element =document.getElementById(`section-${id}`);
+        if (element) element.style.display = id ===sectionId ? "block" :"none";
+    });
+}
+//showUserinformation
+function showUserInformation(user){
+    const informationDiv = document.getElementById("information");
+    informationDiv.innerHTML=`
+        <h3> Your Information </h3>
+        <p><strong>Username:</strong> ${user.name}</p>
+        <p><strong>Password:</strong> ${"*".repeat(user.password.length)}</p>
+        <button id="edit-information-btn">Edit Information</button>
+    
+    `;
+    document.getElementById("edit-information-btn").addEventListener("click",()=>{
+
+    });
+}
